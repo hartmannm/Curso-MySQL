5eb95df002fa7ecd6fa967c76e7f9c46ec87f41f
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-05-31 20:26:55 -0300

    Create 10 - Consultar com join.sql

---
+++ 10 - Consultar com join.sql
@@ -0,0 +1,17 @@
+select 
+	e.nome as 'Estado', 
+    c.nome as 'Cidade', 
+    e.regiao as 'Região'
+from 
+	estados e, cidades c
+where 
+	e.id = c.estado_id;
+
+select 
+	c.nome as 'Cidade',
+    e.nome as 'Estado',
+    e.regiao as 'Região'
+from 
+	estados e
+inner join 
+	cidades c on e.id = c.estado_id;
