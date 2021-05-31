93d07027239970f7b01fcee9107c736359fedc13
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-05-31 19:27:49 -0300

    Update 03 - Consultar estados.sql

--- 03 - Consultar estados.sql
+++ 03 - Consultar estados.sql
@@ -1,11 +1,31 @@
 select * from estados;
 
-select nome, sigla from estados;
+select 
+	nome, 
+    sigla 
+from estados;
 
-select nome as 'Nome do Estado', sigla from estados;
+select 
+	nome as 'Nome do Estado', 
+    sigla 
+from estados;
 
-select nome, sigla from estados where regiao = 'Sul';
+select 
+	nome, 
+    sigla 
+from estados 
+where regiao = 'Sul';
 
-select nome, regiao from estados where populacao >= 10;
+select 
+	nome, 
+    regiao 
+from estados 
+where populacao >= 10;
 
-select nome, regiao from estados where populacao >= 10 order by populacao desc;
+select 
+	nome, 
+    regiao, 
+    populacao
+from estados 
+where populacao >= 10 
+order by populacao desc;
